7b7ffc36145aa68026be84955cc4daaa38513390
feodpv03129 <herlmanoelfernandes@gmail.com>
2023-01-28 13:41:16 -0300

    first commit

---
+++ src/main/java/com/br/userapivalicacaoseguranca/UserApiValicacaoSegurancaApplication.java
@@ -0,0 +1,13 @@
+package com.br.userapivalicacaoseguranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiValicacaoSegurancaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApiValicacaoSegurancaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/userapivalicacaoseguranca/UserApiValicacaoSegurancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.userapivalicacaoseguranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiValicacaoSegurancaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
